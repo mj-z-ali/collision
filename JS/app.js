6850795c078658827d4ba61f827b09cd3cf84bf9
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-05 20:15:56 -0500

    removed fill style and added stroke

--- JS/app.js
+++ JS/app.js
@@ -24,13 +24,13 @@ class Ball	{
 
 		ctx.beginPath();
 
-		ctx.fillStyle = color;
+		ctx.strokeStyle = color;
 
 		ctx.arc(x, y, r, 0, Math.PI * 2);
 
 		ctx.closePath();
 
-		ctx.fill();
+		ctx.stroke();
 	}
 
 	move () {
@@ -83,39 +83,20 @@ class BallFactory {
 }
 
 const balls = new BallFactory;
-balls.generate(2);
-
-const circle = new Ball(undefined, undefined, 50, '#f0f', 0, 0);
-
-const aBall = new Ball(500, 300, 50, '#0f0', 0, 0);
-
-
-canvas.addEventListener('mousemove', (e) => {
-	circle.x = e.clientX;
-	circle.y = e.clientY;
-});
+balls.generate(400);
 
 
 const getDistance = (x1, y1, x2, y2) => {
 	const xDist = x2 - x1;
 	const yDist = y2 - y1;
 
-	return (Math.sqrt((xDist ** 2) + (yDist ** 2))) - 100;
+	return (Math.sqrt((xDist ** 2) + (yDist ** 2))) - 10;
 };
 
 const animate = () => {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	balls.drawAll();
 	// balls.moveAll();
-	circle.draw();
-	aBall.draw();
-	if (getDistance(aBall.x, aBall.y, circle.x, circle.y) < 0) {
-		aBall.color = '#f00';
-	}
-	else {
-		aBall.color = '#0f0';
-	}
-	console.log();
 	requestAnimationFrame(animate);
 };
 
